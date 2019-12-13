86d831a15946cf82d9bd1da59ee3d80002e0b889
youngah <youngah.jeong@edu.sait.ca>
2019-12-12 19:54:30 -0700

    Init flask
    Signed-off-by: youngah <youngah.jeong@edu.sait.ca>

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "main"
+
+@app.route('/index')
+def index():
+    return "index"
+
+if __name__ == '__main__':
+    app.run()
